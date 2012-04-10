d7ced38832d9bf7a7969ebac47b34271a06e95fe
Justin Kramer <jkkramer@gmail.com>
2012-04-10 15:52:09 -0400

    fix clojure 1.3.0 coordinate

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject rate-gate "1.1.0"
   :description "Clojure mechanism for controlling the rate of actions"
-  :dependencies [[org.clojure/clojure "[1.3.0]"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
