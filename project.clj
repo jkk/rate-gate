b9f9262ef823794b7c5c082d802972f8abafecdb
Osbert Feng <osbert@iterinc.com>
2017-10-16 12:28:04 -0700

    Use non-snapshot core.async

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rate-gate "org.clojars.osbert/1.3.4"
+(defproject org.clojars.osber/rate-gate "1.3.5"
   :description "Clojure mechanism for controlling the rate of actions"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [io.rkn/core.async "0.1.0-SNAPSHOT"]])
+                 [org.clojure/core.async "0.2.374"]])
