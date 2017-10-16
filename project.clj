a58bfb64bacd1f837f24e9d82455b504128a54b8
Osbert Feng <osbert@iterinc.com>
2017-10-16 12:35:05 -0700

    Fix typo in group name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.osber/rate-gate "1.3.5"
+(defproject org.clojars.osbert/rate-gate "1.3.5"
   :description "Clojure mechanism for controlling the rate of actions"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.async "0.2.374"]])
